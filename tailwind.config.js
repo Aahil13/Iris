4ec1fe2d4f3d7222695399d3775d2683bc818326
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-08-12 00:40:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        SoftRed: "hsl(7, 99%, 70%)",
+        Yellow: "hsl(51, 100%, 49%)",
+        DarkCyan: "hsl(167, 40%, 24%)",
+        DarkBlue: "hsl(198, 62%, 26%)",
+        moderateCyan: "hsl(168, 34%, 41%)",
+        violet: "hsl(212, 27%, 19%)",
+        VeryDarkblue: "hsl(213, 9%, 39%)",
+        grayishBlue: "hsl(232, 10%, 55%)",
+        GrayishBlue: "hsl(210, 4%, 67%)",
+        White: "hsl(0, 0%, 100%)",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/images/desktop/image-header.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
