82571462fe1679d21e4b8b0dc2cab26a3f38db28
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-08-18 20:16:57 +0100

    refractor: menu event function

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@
 const menuBtn = document.querySelector(".menu");
 const navLinks = document.querySelector(".header-ul");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", function () {
   navLinks.classList.toggle("hidden");
   navLinks.classList.add("mobile-nav");
 });
