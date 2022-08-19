ce8138a3191171719f8fe72d2e5d44d7a1807952
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-08-19 18:37:12 +0100

    Update; add timeline animation for header elements

--- src/gsap.js
+++ src/gsap.js
@@ -1,14 +1,39 @@
-const arrow = document.querySelector(".arrow");
+let t1 = gsap.timeline();
+
+//animate navlink
+t1.from(".links__list", {
+  opacity: 0,
+  stagger: 1,
+  duration: 1,
+  delay: 1.3,
+});
+
+//animate headerbox
+t1.from(".header-box", {
+  duration: 2,
+  opacity: 0,
+  y: "-30px",
+  ease: "power1.in",
+});
 
 // Animate arrow.
 gsap.from(".arrow", {
   rotation: 360,
   x: "65vw",
-  xPercent: -100,
-  duration: 8,
+  xPercent: 100,
+  duration: 10,
   repeat: -1,
+  delay: 18,
   yoyo: true,
   ease: "bounce.inOut",
 });
 
-
+//page navigation animation
+document.querySelectorAll("ul li").forEach((li, index) => {
+  li.addEventListener("click", () => {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: "#section" + (index + 1), offsetY: 0 },
+    });
+  });
+});
