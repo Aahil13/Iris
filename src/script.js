e0b43498b352a9cd46d4a904c2dbfdf20b8aa082
onyeanuna prince eze <onyeanunaprince@gmail.com>
2022-08-19 16:42:59 +0100

    Update: animate h1

--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
 "use strict";
 
+//Menu button
 const menuBtn = document.querySelector(".menu");
 const navLinks = document.querySelector(".header-ul");
 
 menuBtn.addEventListener("click", function () {
   navLinks.classList.toggle("hidden");
   navLinks.classList.add("mobile-nav");
+});
+
+//typed.js
+////////Typewriter effect
+var typed = new Typed(".heading-text", {
+  strings: ["We are creatives", "Iris says Welcome❤"],
+  typeSpeed: 200,
+  backSpeed: 200,
+  loop: true,
 });
